10df1ff956326dde7fb94a6235bc047bfae415ef
Sunilj2911 <suniljaat2911@gmail.com>
2025-01-21 13:26:15 +0530

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,18 +1,8 @@
-/*Create a Structure called BankMgmt with AccNumber, CustName, AvlBalance,
-AccType as members. Implement a Bank management Application as menu driven 
-program for the above said Structure scenario.
-Menu Option:
-1. Withdrawal 2. Deposit 3. Display Balance 4. Exit
-If option
-1 is chosen- Amount can be withdrawn from the account (Withdrawn amount 
-should be given as input). For withdrawal the condition is- the AvlBalance must be 
-greater than withdrawn amount). 
-2 is chosen- Amount can be deposited to the account (the deposited amount 
-should be given as input). The deposited amount should be reflected in AvlBalance of 
-the account.
-3 is chosen- Current available balance (AvlBalance) of the AccNumber should 
-be Displayed with other details
-4 is chosen- Exit from the application.
+/* Write a C program to hold two integer pointers as structure members. Allocate space 
+for the structure and its data members during runtime. Get one array as input. In the 
+second array copy the elements of the first array and replace the odd positioned 
+elements by the product of its adjacent elements. Access the array elements and 
+structures using pointers instead of subscript notation.
  */
 #include <stdio.h>
 #include <stdlib.h>
