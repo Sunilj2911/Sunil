2d600b32da48fefc29716e78c4a07a1787164b85
Sunilj2911 <suniljaat2911@gmail.com>
2025-01-21 12:45:47 +0530

    Delete Assign_04.py

--- Assign_04.py
+++
@@ -1,7 +0,0 @@
-num1=int(input("Enter the number 1:"))
-num2=int(input("Enter the number 2:"))
-num1=num1+num2
-num2=num1-num2
-num1=num1-num2
-print("Swaped value of num1 is ",num1)
-print("Swaped value of num2 is ",num2)    
